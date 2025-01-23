ed4e84dd616720efc5fc39bebb32a63e813b19d3
abbyroyle <123603461+abbyroyle@users.noreply.github.com>
2025-01-23 14:57:59 -0500

    update

---
+++ data/pointbofa.js
@@ -0,0 +1,8 @@
+var bank = {
+"type": "FeatureCollection",
+"name": "bofapoint1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": bank }, "geometry": { "type": "Point", "coordinates": [ -80.842218846394346, 35.227287858250492 ] } }
+]
+}
